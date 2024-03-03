371e5692774496813be2cb3a59a66c9a255197ab
Hayley Ewers <hayleyewers@hotmail.co.uk>
2024-03-03 16:52:23 +0000

    Registration setup to database

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 import os
-from flask import Flask, render_template, url_for, request,  redirect
+from flask import (
+    Flask, render_template, url_for, 
+    redirect, flash, request, redirect, session)
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
-from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 
 @app.route("/")
@@ -21,8 +33,24 @@ def log_in():
 
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def  register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash ("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username":request.form.get("username").lower(),
+            "password" :generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("You have successfully registered!")
     return render_template("register.html")
 
 
@@ -33,4 +61,5 @@ if  __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=True
               )
+
 
